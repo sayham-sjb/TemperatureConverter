daa4a4c3e4f8c5fcd3990fc8d62c14a9a358d0fd
Sayham Chowdhury <sayham28@gmail.com>
2020-01-19 00:03:10 -0500

    fx(TemperatureConverter.js): add js file

---
+++ src/TemperatureConverter.js
@@ -0,0 +1,12 @@
+function CelsiusConvert() {
+    TempVS.inFah.value = (TempVS.inCel.value * 9 / 5) + 32 + (' \xB0F');
+    TempVS.inKel.value = TempVS.inCel.value * 1 + 273.15 + (' K');
+}
+function FahrenheitConvert() {
+    TempVS.inCel.value = (TempVS.inFah.value - 32) * 5 / 9 + (' \xB0C');
+    TempVS.inKel.value = ((TempVS.inFah.value - 32) * 5 / 9) + 273.15 + (' K');
+}
+function KelvinConvert() {
+    TempVS.inCel.value = TempVS.inKel.value - 273.15 + (' \xB0C');
+    TempVS.inFah.value = ((TempVS.inKel.value - 273.15) * 9 / 5) + 32 + (' \xB0F');
+}
